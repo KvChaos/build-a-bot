762876ff1a0d4b78607a1bd4d8a95d61861c9727
Cory Newby <kvchaos@gmail.com>
2019-03-12 18:21:30 +0000

    Updated through module:  Enabling Inter-component Communication

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="content">
+    <div class="preview">
+        <CollapsibleSection>
+          <div class="preview-content">
+            <div class="top-row">
+              <img :src="selectedRobot.head.src" />
+            </div>
+            <div class="middle-row">
+              <img :src="selectedRobot.leftArm.src" class="rotate-left" />
+              <img :src="selectedRobot.torso.src" />
+              <img :src="selectedRobot.rightArm.src" class="rotate-right" />
+            </div>
+            <div class="bottom-row">
+              <img :src="selectedRobot.base.src"/>
+            </div>
+        </div>
+    </CollapsibleSection>
     <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
+    </div>    
     <div class="top-row">
       <div class="top part" :class="[saleBorderClass]">
         <!--<div class="robot-name">-->
@@ -59,12 +76,13 @@
 <script>
   import availableParts from "../data/parts";
   import createdHookMixin from './created-hook-mixin';
-  import PartSelector from './PartSelector.vue'
+  import PartSelector from './PartSelector.vue';
+  import CollapsibleSection from '../shared/CollapsibleSection.vue'
 
 
   export default {
     name: "RobotBuilder",
-    components: { PartSelector },
+    components: { PartSelector, CollapsibleSection },
     mixins: [createdHookMixin],
     data() {
       return {
@@ -227,8 +245,7 @@
 
   .add-to-cart {
     position: absolute;
-    right: 30px;
-    width: 220px;
+    width: 210px;
     padding: 3px;
     font-size: 16px;
   }
@@ -246,5 +263,31 @@
 
   .sale-border {
     border: solid red 3px;
+  }
+
+  .preview {
+    position: absolute;
+    top: -20px;
+    right: 0;
+    width: 210px;
+    height: 210px;
+    padding: 5px;
+  }
+
+  .preview-content {
+    border: 1px solid #999;
+  }
+
+  .preview img {
+    width: 50px;
+    height: 50px;
+  }
+
+  .rotate-right {
+    transform: rotate(90deg);
+  }
+
+  .rotate-left {
+    transform: rotate(-90deg);
   }
 </style>
